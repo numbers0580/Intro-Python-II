228c9a54a4166939aa3c771af5ca69fa7b324956
numbers0580 <koreankid18@yahoo.com>
2020-09-02 16:42:37 -0600

    In progress

--- src/room.py
+++ src/room.py
@@ -1,2 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __mymethod__(self, name):
+        print(f"My name is {self.name}. I know that x is {self.x} and y is {self.y}")
+
+    def call_mymethod(self, name):
+        self.__mymethod__(name)
